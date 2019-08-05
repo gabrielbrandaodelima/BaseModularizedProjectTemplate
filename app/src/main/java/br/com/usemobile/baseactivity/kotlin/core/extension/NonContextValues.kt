f50e4c86227a34416e7ca7342ded82e4a6f716d2
Gabriel B. Lima <gabriel.brandao@usemobile.xyz>
2019-08-05 11:57:24 -0300

    #Feat: non Ctx values

---
+++ app/src/main/java/br/com/usemobile/baseactivity/kotlin/core/extension/NonContextValues.kt
@@ -0,0 +1,9 @@
+package br.com.usemobile.baseactivity.kotlin.core.extension
+
+/** Created by Gabriel
+ * on 29/04/2019
+ * at 08:35
+ * at Usemobile
+ **/
+const val activityMenu = "TELA PRINCIPAL"
+const val activityLogin = "TELA LOGIN"
