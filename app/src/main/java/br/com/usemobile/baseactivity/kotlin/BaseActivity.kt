ec4a8a0a511cf43ba55c1003d932a817b5753b5f
Gabriel B. Lima <gabriel.brandao@usemobile.xyz>
2019-08-05 09:52:41 -0300

    #First Commit: Created Project

---
+++ app/src/main/java/br/com/usemobile/baseactivity/kotlin/BaseActivity.kt
@@ -0,0 +1,36 @@
+package br.com.usemobile.baseactivity.kotlin
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.Menu
+import android.view.MenuItem
+
+import kotlinx.android.synthetic.main.toolbar.*
+
+class BaseActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar_geral)
+
+
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
